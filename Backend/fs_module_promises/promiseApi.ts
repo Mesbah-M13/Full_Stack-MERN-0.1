3e963fe0b3402de456e64b3b2a3e240965c00290
Mesbah-M13 <mesbah0010@gmail.com>
2025-11-09 09:47:10 +0600

    add try catch in fs promise api

--- Backend/fs_module_promises/promiseApi.ts
+++ Backend/fs_module_promises/promiseApi.ts
@@ -1,8 +1,12 @@
 import * as fs from "node:fs/promises";
 
 async function createFile(pathName: string) {
-  await fs.writeFile(pathName, "Hello promise api \n");
-  await fs.appendFile(pathName, "Hello promise api using nodejs");
+  try {
+    await fs.writeFile(pathName, "Hello promise api... \n");
+    await fs.appendFile(pathName, "Hello promise api using nodejs...");
+  } catch (err) {
+    console.log("Error found", err);
+  }
   console.log("File written!");
 }
 createFile("./file1.txt");
