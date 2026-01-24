0de067cc088438ad11dc211cd2fc0e4cbddb0d7f
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-24 22:23:31 +0600

    04_modules

---
+++ Backend/piyush/modules/add.js
@@ -0,0 +1,11 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+function sub(n1, n2) {
+  return n1 - n2;
+}
+
+module.exports = {
+  add,
+  sub,
+};
---
+++ Backend/piyush/modules/app.js
@@ -0,0 +1,4 @@
+const math = require("./add");
+
+console.log(math.add(6, 14));
+console.log(math.sub(100, 1));
