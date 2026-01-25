05bdaa25ad416f01440487009f8b2760405f75ea
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-25 07:23:18 +0600

    2 ways to import modules

--- Backend/piyush/modules/app.js
+++ Backend/piyush/modules/app.js
@@ -1,4 +1,10 @@
-const math = require("./add");
+const { add, sub } = require("./add");
 
-console.log(math.add(6, 14));
-console.log(math.sub(100, 1));
+console.log(`Result after addition: `, add(6, 14));
+console.log(`Result after subtraction: `, sub(100, 1));
+
+// other way
+const calc = require("./add");
+
+console.log(`addition result:`, calc.add(50, 20));
+console.log(`subtraction result:`, calc.sub(50, 20));
