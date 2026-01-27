2dc2829029661e188dcaf2340078ef1d98a6f48b
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-27 20:12:43 +0600

    delete file using unlinkSync by fs module

--- Backend/piyush/appendFile/main.js
+++ Backend/piyush/appendFile/main.js
@@ -3,4 +3,7 @@ const fs = require("fs");
 fs.appendFileSync("newFile.txt", `${new Date().toString()}\n`);
 
 fs.copyFileSync("./newFile.txt", "./copyFile.txt");
-console.log("Date appended successfully");
+console.log("Date appended & copied successfully");
+
+fs.unlinkSync("./copyFile.txt");
+console.log("data deleted ....");
