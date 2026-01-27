d6ba22655c90acc031a29881db4f61a4b09138ed
Mesbah-M13 <mesbah0010@gmail.com>
2026-01-27 20:06:22 +0600

    copyFileSync to copy file using fs module

--- Backend/piyush/appendFile/main.js
+++ Backend/piyush/appendFile/main.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
 
 fs.appendFileSync("newFile.txt", `${new Date().toString()}\n`);
+
+fs.copyFileSync("./newFile.txt", "./copyFile.txt");
 console.log("Date appended successfully");
