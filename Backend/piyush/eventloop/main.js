27ffc6b5b82552a029663fb2aa7d748177f272ce
Mesbah-M13 <mesbah0010@gmail.com>
2026-02-01 23:02:00 +0600

    blocking & non-blocking operation in node js

--- Backend/piyush/eventloop/main.js
+++ Backend/piyush/eventloop/main.js
@@ -1,3 +1,17 @@
-const os = require("os");
+const fs = require("fs");
+console.log(1);
+console.log(fs.readFileSync("test.txt", "utf-8"));
+console.log(2);
 
-console.log(os.cpus().length);
+// checking how many cores my system has
+console.log(3);
+
+fs.readFile("./test.txt", "utf-8", (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
+});
+console.log(4);
+
+// const os = require("os");
+
+// console.log(os.cpus().length);
