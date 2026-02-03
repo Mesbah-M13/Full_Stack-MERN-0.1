a63a4007d6ab75be2918c9e9d4bdbb792ec94687
Mesbah-M13 <mesbah0010@gmail.com>
2026-02-03 08:33:55 +0600

    log req using fs & http

--- Backend/piyush/server/index.js
+++ Backend/piyush/server/index.js
@@ -1,9 +1,14 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = 3000;
 const myServer = http.createServer((req, res) => {
-  console.log("New req rec.");
-  res.end("Hello from Server");
+  const log = `${Date.now()}: New Req Received\n `;
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) console.log(err);
+    else res.end("Hello from server, date received");
+  });
+  // res.end("Hello from Server");
 });
 
 myServer.listen(PORT, () => console.log(`Server running at ${PORT}`));
