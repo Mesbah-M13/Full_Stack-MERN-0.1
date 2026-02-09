996971ff4339a71cf0383716d99def80cb2bb9bd
Mesbah-M13 <mesbah0010@gmail.com>
2026-02-09 23:52:14 +0600

    query parameter using searchParams

--- Backend/piyush/url/main.js
+++ Backend/piyush/url/main.js
@@ -1,10 +1,18 @@
-let url = require("url");
-let adr = "http://localhost:8080/default.htm?year=2017&month=february";
-let q = url.parse(adr, true);
+// let url = require("url");
+// let adr = "http://localhost:8080/default.htm?year=2017&month=february";
+// let q = url.parse(adr, true);
 
-console.log(q.host);
-console.log(q.pathname);
-console.log(q.search);
+// console.log(q.host);
+// console.log(q.pathname);
+// console.log(q.search);
 
-let qdata = q.query;
-console.log(qdata.month);
+// let qdata = q.query;
+// console.log(qdata.month);
+
+const myURL = new URL("https://example.com?name=John&age=25");
+
+console.log(myURL.searchParams.get("name")); // John
+console.log(myURL.searchParams.get("age")); // 25
+
+myURL.searchParams.append("city", "Dhaka");
+console.log(myURL.toString());
